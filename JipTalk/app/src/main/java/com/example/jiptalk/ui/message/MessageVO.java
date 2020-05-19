b9477117598a1e695a082023e55340a25c0aa6ba
JHM9191 <hm.jo.9191@gmail.com>
2020-05-20 05:34:45 +0900

    Added and Modified MessageFragment.java, MessageDetailActivity.java, etc by JHM9191 5/20

---
+++ JipTalk/app/src/main/java/com/example/jiptalk/ui/message/MessageVO.java
@@ -0,0 +1,62 @@
+package com.example.jiptalk.ui.message;
+
+public class MessageVO {
+
+    String name;
+    String title;
+    String content;
+    String time;
+
+
+    public MessageVO() {
+    }
+
+    public MessageVO(String name, String title, String content, String time) {
+        this.name = name;
+        this.title = title;
+        this.content = content;
+        this.time = time;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    @Override
+    public String toString() {
+        return "MessageVO{" +
+                "name='" + name + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", time='" + time + '\'' +
+                '}';
+    }
+}
