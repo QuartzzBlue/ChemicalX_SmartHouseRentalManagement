007a91b44038ca6e1088421020838d856a8c176b
seule95 <seulee95@naver.com>
2020-05-20 00:13:35 +0900

    Revert "modify splash image by Seul 05/19"
    This reverts commit b6bebd4947a2756e10f403f5e755d2d38f8c2f2d.

--- JipTalk/app/src/main/java/com/example/jiptalk/SplashActivity.java
+++ JipTalk/app/src/main/java/com/example/jiptalk/SplashActivity.java
@@ -2,21 +2,20 @@ package com.example.jiptalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.jiptalk.R;
 
-public class SplashActivity extends Activity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //getSupportActionBar().hide();
+        getSupportActionBar().hide();
 
         Handler handler = new Handler();
 
