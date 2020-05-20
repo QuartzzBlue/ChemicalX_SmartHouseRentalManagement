7294de9de7ebd6ba58b8c33b688e78b74d7aa04f
yeogenius <who3637@naver.com>
2020-05-20 22:23:36 +0900

    Update Home Fragment Highlight by Yeojin 05/20

---
+++ JipTalk/app/src/main/java/com/example/jiptalk/ui/home/Chart1Fragment.java
@@ -0,0 +1,44 @@
+package com.example.jiptalk.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.jiptalk.R;
+
+public class Chart1Fragment extends Fragment {
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static Chart1Fragment newInstance(int page, String title) {
+        Chart1Fragment fragment = new Chart1Fragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.highlight_chart1, container, false);
+        return view;
+    }
+}
