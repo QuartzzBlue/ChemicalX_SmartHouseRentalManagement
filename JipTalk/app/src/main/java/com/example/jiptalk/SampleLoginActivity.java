a515dab5a9e212854dd996ac89a53eab242e9e75
Seul <seulee95@naver.com>
2020-05-25 23:36:05 +0900

    Firebase Phonenumber Authentification by Seul 05/25

--- JipTalk/app/src/main/java/com/example/jiptalk/SampleLoginActivity.java
+++ JipTalk/app/src/main/java/com/example/jiptalk/SampleLoginActivity.java
@@ -3,11 +3,18 @@ package com.example.jiptalk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SampleLoginActivity extends AppCompatActivity {
     TextView userId, userUid;
+    Button signOutBt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +23,61 @@ public class SampleLoginActivity extends AppCompatActivity {
 
         userId = findViewById(R.id.loginUserId);
         userUid = findViewById(R.id.loginUserUid);
+        signOutBt = findViewById(R.id.sample_signoutBt);
 
         Intent intent = getIntent();
         String userID = intent.getExtras().getString("userID"); /*String형*/
         String userUID = intent.getExtras().getString("userUID"); /*String형*/
-        userId.setText(userID);
-        userUid.setText(userUID);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // Check if user's email is verified
+            boolean emailVerified = user.isEmailVerified();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getIdToken() instead.
+            String uid = user.getUid();
+
+            userId.setText(name + ", " + email + ", " + emailVerified);
+            userUid.setText(uid);
+        }
+
+
+//        userId.setText(name);
+//        userUid.setText(userUID);
+
+        signOutBt.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    // Name, email address, and profile photo Url
+                    String name = user.getDisplayName();
+                    String email = user.getEmail();
+                    Uri photoUrl = user.getPhotoUrl();
+
+                    // Check if user's email is verified
+                    boolean emailVerified = user.isEmailVerified();
+
+                    // The user's ID, unique to the Firebase project. Do NOT use this value to
+                    // authenticate with your backend server, if you have one. Use
+                    // FirebaseUser.getIdToken() instead.
+                    String uid = user.getUid();
+
+                    userId.setText(name + ", " + email + ", " + emailVerified);
+                    userUid.setText(uid);
+                }
+            }
+        });
 
     }
 }
