998ad35d7fd7838c6090f6fa875bddf806f218cc
yeogenius <who3637@naver.com>
2020-06-13 03:09:59 +0900

    Make getData process to be syncronous by Yeojin 06/13

--- JipTalk/app/src/main/java/com/example/jiptalk/SplashActivity.java
+++ JipTalk/app/src/main/java/com/example/jiptalk/SplashActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
@@ -21,6 +23,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class SplashActivity extends Activity {
+
+    private static final String TAG = "SplashActivity";
     private boolean autoLoginFlag;
     private Intent intent;
 
@@ -49,74 +53,102 @@ public class SplashActivity extends Activity {
 //        }, 2000);
 
         if(autoLoginFlag){
-            MyRunnable myRunnable = new MyRunnable();
-            Thread getDataThread = new Thread(myRunnable);
-            getDataThread.start();
-            try {
-                getDataThread.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            intent = new Intent(getApplicationContext(), MainActivity.class);
-        }else {
-            intent = new Intent(getApplicationContext(), LoginActivity.class);
-        }
 
-        startActivity(intent);
-        finish();
+            //1.
+//            MyRunnable myRunnable = new MyRunnable();
+//                Thread getDataThread = new Thread(myRunnable);
+//                getDataThread.start();
+//                Log.v(TAG,"getDataThread Starts");
+//                try {
+//                    getDataThread.join();
+//                Log.v(TAG,"getDataThread joined");
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//intent = new Intent(getApplicationContext(), MainActivity.class);
+
+            //2.
+//            AsyncTask<Void, Void, Void> getDataTask = new AsyncTask<Void, Void, Void>() {
+//
+//                @Override
+//                protected void onPreExecute() {
+//                    super.onPreExecute();
+//                }
+//
+//                @Override
+//                protected void onPostExecute(Void aVoid) {
+//                    super.onPostExecute(aVoid);
+//                    Log.v(TAG, "Move To MainActivity");
+//                    intent = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                }
+//
+//                @Override
+//                protected Void doInBackground(Void... voids) {
+//                    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+//                    DatabaseReference buildingReference = firebaseDatabase.getReference("buildings");
+//                    FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+//
+//                    Constant.userUID = fUser.getUid();
+//                    Constant.userID = fUser.getEmail();
+//
+//                    buildingReference.child(Constant.userUID).addValueEventListener(new ValueEventListener() {
+//
+//                        @Override
+//                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                            Log.v(TAG, "InitConstants() : onDataChange");
+//
+//                            for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                                Building buildingItem = postSnapshot.getValue(Building.class);
+//                                buildingItem.setId(postSnapshot.getKey());
+//                                Constant.buildings.put(postSnapshot.getKey(), buildingItem);
+//                            }
+//
+//                            if (Constant.buildings != null) {
+//                                Log.v(TAG, "Set Constant.building : " + Constant.buildings.toString());
+//                                Log.v(TAG, "InitConstants() : succeed");
+//                            }
+//                        }
+//
+//                        @Override
+//                        public void onCancelled(@NonNull DatabaseError databaseError) {
+//                            Log.v(TAG, "InitConstants() : onCancelled", databaseError.toException());
+//                        }
+//
+//                    });
+//                    return null;
+//                }
+//            }.execute();
+
+            new DataSync(this, new DataSync.OnTaskFinishedCallback() {
+                @Override
+                public void onTaskFinished() {
+                    Log.v(TAG, "Move To MainActivity");
+
+                        intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    
+
+                }
+            }).execute();
+
+        }else {
+            Log.v(TAG,"Move To LoginActivity");
+            intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
 
 
     }
 
-   class MyRunnable implements Runnable{
-
-       @Override
-       public void run() {
-           FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-           DatabaseReference buildingReference = firebaseDatabase.getReference("buildings");
-//        DatabaseReference userReference = firebaseDatabase.getReference("user");
-
-           FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
-
-
-           Constant.userUID = fUser.getUid();
-           Constant.userID = fUser.getEmail();
-
-           buildingReference.child(Constant.userUID).addValueEventListener(new ValueEventListener() {
-
-               @Override
-               public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                   Log.w("===", "InitConstants() : onDataChange");
-
-                   for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                       Building buildingItem = postSnapshot.getValue(Building.class);
-                       buildingItem.setId(postSnapshot.getKey());
-                       Constant.buildings.put(postSnapshot.getKey(),buildingItem);
-                   }
-
-                   if (Constant.buildings != null) {
-                       Log.w("===", "Set Constant.building : " + Constant.buildings.toString());
-                       Log.w("===", "InitConstants() : succeed");
-                   }
-
-               }
-
-               @Override
-               public void onCancelled(@NonNull DatabaseError databaseError) {
-                   Log.w("===", "InitConstants() : onCancelled", databaseError.toException());
-               }
-
-           });
-
-       }
-
-    };
-
     private void isAutoLogin(){
         SharedPreferences appData = getSharedPreferences("appData",MODE_PRIVATE);
         autoLoginFlag = appData.getBoolean("SAVE_LOGIN_DATA",false);
-        Log.d("===","AutoLogin : " + autoLoginFlag);
+        Log.v(TAG,"AutoLogin : " + autoLoginFlag);
         String email = appData.getString("email","");
         String password = appData.getString("password","");
         return;
@@ -133,5 +165,72 @@ public class SplashActivity extends Activity {
     public void onBackPressed() {
         /* 스플래시 화면에서 뒤로가기 기능 제거. */
     }
-
 }
+
+
+
+class DataSync extends AsyncTask<Void,Integer,Void>{
+
+        private static final String TAG = "SplashActivity";
+        private Context context;
+        private OnTaskFinishedCallback callback;
+
+        // Custom callbac
+        public interface OnTaskFinishedCallback {
+            void onTaskFinished();
+        }
+
+        public DataSync(final Context context, OnTaskFinishedCallback callback){
+            this.context = context;
+            this.callback = callback;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            DatabaseReference buildingReference = firebaseDatabase.getReference("buildings");
+            FirebaseUser fUser = FirebaseAuth.getInstance().getCurrentUser();
+
+            Constant.userUID = fUser.getUid();
+            Constant.userID = fUser.getEmail();
+
+            buildingReference.child(Constant.userUID).addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Log.v(TAG, "InitConstants() : onDataChange");
+
+                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                        Building buildingItem = postSnapshot.getValue(Building.class);
+                        buildingItem.setId(postSnapshot.getKey());
+                        Constant.buildings.put(postSnapshot.getKey(), buildingItem);
+                    }
+
+                    if (Constant.buildings != null) {
+                        Log.v(TAG, "Set Constant.building : " + Constant.buildings.toString());
+                        Log.v(TAG, "InitConstants() : succeed");
+                        callback.onTaskFinished();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                    Log.v(TAG, "InitConstants() : onCancelled", databaseError.toException());
+                }
+
+            });
+            return null;
+        }
+}
+
+
