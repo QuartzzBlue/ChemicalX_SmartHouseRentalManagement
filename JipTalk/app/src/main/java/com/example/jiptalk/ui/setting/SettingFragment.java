7749f635f89109b204b8aff0448d77def1a0dd74
Seul <seulee95@naver.com>
2020-06-22 19:47:44 +0900

    Modifying unitDetail Appbar by Seul 06/22

--- JipTalk/app/src/main/java/com/example/jiptalk/ui/setting/SettingFragment.java
+++ JipTalk/app/src/main/java/com/example/jiptalk/ui/setting/SettingFragment.java
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -25,7 +26,7 @@ public class SettingFragment extends Fragment {
     private ViewGroup viewGroup;
     private SettingViewModel mViewModel;
     private Button alarmBt, infoBt, developersBt, logoutBt;
-    private Intent intent;
+    private Intent logoutIntent;
     private Context nowContext;
 
     public static SettingFragment newInstance() {
@@ -42,24 +43,24 @@ public class SettingFragment extends Fragment {
         alarmBt.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                intent = new Intent(nowContext, SettingAlarmActivity.class);
-                startActivity(intent);
+                logoutIntent = new Intent(nowContext, SettingAlarmActivity.class);
+                startActivity(logoutIntent);
             }
         });
 
         infoBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(nowContext, SettingUserInfoActivity.class);
-                startActivity(intent);
+                logoutIntent = new Intent(nowContext, SettingUserInfoActivity.class);
+                startActivity(logoutIntent);
             }
         });
 
         developersBt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(nowContext, SettingDevelopersActivity.class);
-                startActivity(intent);
+                logoutIntent = new Intent(nowContext, SettingDevelopersActivity.class);
+                startActivity(logoutIntent);
             }
         });
 
@@ -69,10 +70,16 @@ public class SettingFragment extends Fragment {
                 /** 로그 아웃 **/
                 FirebaseAuth.getInstance().signOut();
 
-                intent = new Intent(nowContext, LoginActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //중간 스택에 쌓인 액티비티들 제거
-                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); //액티비티가 스택 맨 위에 실행중이라면 재사용
-                startActivity(intent);
+                // 자동로그인 초기화
+                SharedPreferences appData = getActivity().getSharedPreferences("appData",Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = appData.edit();
+                editor.clear();
+                editor.commit();
+
+                logoutIntent = new Intent(nowContext, LoginActivity.class);
+                logoutIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //중간 스택에 쌓인 액티비티들 제거
+                logoutIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP); //액티비티가 스택 맨 위에 실행중이라면 재사용
+                startActivity(logoutIntent);
                 Toast.makeText(nowContext, "로그아웃 되었습니다.", Toast.LENGTH_SHORT).show();
                 getActivity().finish();
             }
